2f5428c3f12596c7f962ba8e3f75b3198320ec08
P4ndro <sandro02050502@gmail.com>
2026-01-01 23:59:53 +0400

    Add CORS and Clerk middleware to Express server; update environment configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import cors from "cors";
 import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
 
 
 const app = express();
 
+app.use(clerkMiddleware());
+app.use(cors({ origin: ENV.FRONTEND_URL}))
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
+
+ 
     res.json({ message: "Hello World!" 
         ,
         endpoints: {
